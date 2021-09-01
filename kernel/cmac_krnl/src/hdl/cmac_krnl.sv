1a1c3114d0fc5142cbff255e35e2dd0ffcf8bfb3
zhe <zhe@ethz.ch>
2021-09-01 10:44:17 +0200

    fix xrt issue

--- kernel/cmac_krnl/src/hdl/cmac_krnl.sv
+++ kernel/cmac_krnl/src/hdl/cmac_krnl.sv
@@ -14,34 +14,6 @@ module cmac_krnl #(
   // System Signals
   input  wire                                    ap_clk               ,
   input  wire                                    ap_rst_n             ,
-  // input  wire                                                   ap_clk_2                            ,
-  // input  wire                                                   ap_rst_n_2                          ,
-  //  Note: A minimum subset of AXI4 memory mapped signals are declared.  AXI
-  // signals omitted from these interfaces are automatically inferred with the
-  // optimal values for Xilinx accleration platforms.  This allows Xilinx AXI4 Interconnects
-  // within the system to be optimized by removing logic for AXI4 protocol
-  // features that are not necessary. When adapting AXI4 masters within the RTL
-  // kernel that have signals not declared below, it is suitable to add the
-  // signals to the declarations below to connect them to the AXI4 Master.
-  // 
-  // List of ommited signals - effect
-  // -------------------------------
-  // ID - Transaction ID are used for multithreading and out of order
-  // transactions.  This increases complexity. This saves logic and increases Fmax
-  // in the system when ommited.
-  // SIZE - Default value is log2(data width in bytes). Needed for subsize bursts.
-  // This saves logic and increases Fmax in the system when ommited.
-  // BURST - Default value (0b01) is incremental.  Wrap and fixed bursts are not
-  // recommended. This saves logic and increases Fmax in the system when ommited.
-  // LOCK - Not supported in AXI4
-  // CACHE - Default value (0b0011) allows modifiable transactions. No benefit to
-  // changing this.
-  // PROT - Has no effect in current acceleration platforms.
-  // QOS - Has no effect in current acceleration platforms.
-  // REGION - Has no effect in current acceleration platforms.
-  // USER - Has no effect in current acceleration platforms.
-  // RESP - Not useful in most acceleration platforms.
-  // 
   // AXI4-Stream (master) interface axis_net_rx
   output wire                                    axis_net_rx_tvalid   ,
   input  wire                                    axis_net_rx_tready   ,
@@ -54,24 +26,6 @@ module cmac_krnl #(
   input  wire [C_AXIS_NET_TX_TDATA_WIDTH-1:0]    axis_net_tx_tdata    ,
   input  wire [C_AXIS_NET_TX_TDATA_WIDTH/8-1:0]  axis_net_tx_tkeep    ,
   input  wire                                    axis_net_tx_tlast    ,
-  // AXI4-Lite slave interface
-  input  wire                                    s_axi_control_awvalid,
-  output wire                                    s_axi_control_awready,
-  input  wire [C_S_AXI_CONTROL_ADDR_WIDTH-1:0]   s_axi_control_awaddr ,
-  input  wire                                    s_axi_control_wvalid ,
-  output wire                                    s_axi_control_wready ,
-  input  wire [C_S_AXI_CONTROL_DATA_WIDTH-1:0]   s_axi_control_wdata  ,
-  input  wire [C_S_AXI_CONTROL_DATA_WIDTH/8-1:0] s_axi_control_wstrb  ,
-  input  wire                                    s_axi_control_arvalid,
-  output wire                                    s_axi_control_arready,
-  input  wire [C_S_AXI_CONTROL_ADDR_WIDTH-1:0]   s_axi_control_araddr ,
-  output wire                                    s_axi_control_rvalid ,
-  input  wire                                    s_axi_control_rready ,
-  output wire [C_S_AXI_CONTROL_DATA_WIDTH-1:0]   s_axi_control_rdata  ,
-  output wire [2-1:0]                            s_axi_control_rresp  ,
-  output wire                                    s_axi_control_bvalid ,
-  input  wire                                    s_axi_control_bready ,
-  output wire [2-1:0]                            s_axi_control_bresp  ,
 
   // Network physical
   input  wire clk_gt_freerun,
@@ -97,39 +51,6 @@ reg                                 areset                         = 1'b0;
 always @(posedge ap_clk) begin
   areset <= ~ap_rst_n;
 end
-
-///////////////////////////////////////////////////////////////////////////////
-// Begin control interface RTL.  Modifying not recommended.
-///////////////////////////////////////////////////////////////////////////////
-
-
-// AXI4-Lite slave interface
-cmac_krnl_control_s_axi #(
-  .C_S_AXI_ADDR_WIDTH ( C_S_AXI_CONTROL_ADDR_WIDTH ),
-  .C_S_AXI_DATA_WIDTH ( C_S_AXI_CONTROL_DATA_WIDTH )
-)
-inst_control_s_axi (
-  .ACLK    ( ap_clk                ),
-  .ARESET  ( areset                ),
-  .ACLK_EN ( 1'b1                  ),
-  .AWVALID ( s_axi_control_awvalid ),
-  .AWREADY ( s_axi_control_awready ),
-  .AWADDR  ( s_axi_control_awaddr  ),
-  .WVALID  ( s_axi_control_wvalid  ),
-  .WREADY  ( s_axi_control_wready  ),
-  .WDATA   ( s_axi_control_wdata   ),
-  .WSTRB   ( s_axi_control_wstrb   ),
-  .ARVALID ( s_axi_control_arvalid ),
-  .ARREADY ( s_axi_control_arready ),
-  .ARADDR  ( s_axi_control_araddr  ),
-  .RVALID  ( s_axi_control_rvalid  ),
-  .RREADY  ( s_axi_control_rready  ),
-  .RDATA   ( s_axi_control_rdata   ),
-  .RRESP   ( s_axi_control_rresp   ),
-  .BVALID  ( s_axi_control_bvalid  ),
-  .BREADY  ( s_axi_control_bready  ),
-  .BRESP   ( s_axi_control_bresp   )
-);
 
 ///////////////////////////////////////////////////////////////////////////////
 // Add kernel logic here.  Modify/remove example code as necessary.
